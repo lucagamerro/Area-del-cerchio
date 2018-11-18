7b256da22e16afdd868a406b0781cafb58cc049a
Luca Gamerro <43759024+lucagamerro@users.noreply.github.com>
2018-11-18 21:45:24 +0100

    Create area del cerchio.py

---
+++ area del cerchio.py
@@ -0,0 +1,13 @@
+print('________________________________________')
+print('CIRCONFERENZA DEL CERCHIO DATO IL RAGGIO')
+ 
+raggio = input('Inserire il raggio: ')
+
+r = float(raggio)
+
+print('Diametro' , r*2)
+print('Circonferenza' , r*2 * 3.14)
+print('Area' , r**2 * 3.14)
+
+print('ho ragione, lo so')
+print('Autore: Luca Gamerro' , 'Linguaggio di programmazione: Python')
